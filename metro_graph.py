4bdd72c063ab95674c149379e159986cfb1664a4
Saiman Saxena <saimanhero1555@gmail.com>
2025-03-07 14:35:24 +0530

    Update metro_graph.py

--- metro_graph.py
+++ metro_graph.py
@@ -2,9 +2,35 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import metro_data as md
 
-num = 1
-graph = md.graph 
+graph = md.graph
+lines = md.lines
 
-for stations in graph:
-    num += 1
-    print(stations , num )  
+def shortest_path_function(graph):
+    number_of_stations = 0
+    input_start = input("Enter the starting station: ").lower()
+    input_end = input("Enter the destination station: ").lower()
+
+    if input_start not in graph.nodes:
+        print(f"{input_start} is not a valid station.")
+        return
+    if input_end not in graph.nodes:
+        print(f"{input_end} is not a valid station.")
+        return
+    
+    shortest_path = nx.dijkstra_path(graph,input_start,input_end)
+    for i in shortest_path:
+        number_of_stations += 1
+    print(f"The shortest path between {input_start} and {input_end} is: {shortest_path}")
+    for stations in shortest_path:
+        print(stations)
+    print(f"The number of stations between {input_start} and {input_end} is: {number_of_stations}")
+    time_of_travel(shortest_path)
+
+def time_of_travel(shortest_path):
+        time = 0
+        for i in shortest_path:
+             time = shortest_path[i].weight 
+        print(f"The time is :{time}")
+
+
+shortest_path_function(graph)
